772bdc8d957269b516b44465c8f86d58b8471d15
OzzCo <rafaelHerrera360@outlook.com>
2020-06-14 18:02:05 -0600

    no me acuerdo lo que cambie

--- app/src/main/java/hn/edu/ujcv/myhelloworldapplication/controllers/MainActivity.kt
+++ app/src/main/java/hn/edu/ujcv/myhelloworldapplication/controllers/MainActivity.kt
@@ -27,13 +27,66 @@ class MainActivity : AppCompatActivity() {
         mitexto.setText( model.display.toString())
     }
 
+    fun twoPressed(view : View){
+        model.twoPressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun threePressed(view : View){
+        model.threePressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun fourPressed(view : View){
+        model.fourPressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun fivePressed(view : View){
+        model.fivePressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun sixpressed(view : View){
+        model.sixPressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun sevenPressed(view : View){
+        model.sevenPressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun eigthPressed(view : View){
+        model.eigthPressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun ninePressed(view : View){
+        model.ninePressed()
+        mitexto.setText( model.display.toString())
+    }
+    fun zeroPressed(view : View){
+        model.zeroPressed()
+        mitexto.setText( model.display.toString())
+    }
+
     fun sumPressed(view:View){
         model.sumPressed()
         mitexto.text = model.display.toString()
     }
 
+    fun subtractionPressed(view:View){
+        model.subtractionPressed()
+        mitexto.text = model.display.toString()
+    }
+    fun multiplicationPressed(view:View){
+        model.multiplicationPressed()
+        mitexto.text = model.display.toString()
+    }
     fun equalsPressed(view :View){
         model.equalsPressed()
+        mitexto.text = model.display.toString()
+    }
+    fun divisionPressed(view :View){
+        model.divisionPressed()
+        mitexto.text = model.display.toString()
+    }
+    fun acPressed(view :View){
+        model.acPressed()
         mitexto.text = model.display.toString()
     }
 
--- app/src/main/java/hn/edu/ujcv/myhelloworldapplication/models/CalculatorModel.kt
+++ app/src/main/java/hn/edu/ujcv/myhelloworldapplication/models/CalculatorModel.kt
@@ -5,54 +5,150 @@ class CalculatorModel {
     //constantes de operaciones
     val none = 0;
     val sum = 1;
-    val resta = 2;
+    val subs = 2;
     val mul = 4;
+    val div = 5;
 
     // variables que llevan el estado de la calculadora
     var display: Double = 0.0
     var operando1: Double = 0.0
     var operando2: Double = 0.0
     var operacion: Int = 0
+    var resultado: Double = 0.0
     var willClear = false;
 
-
-
-    //metodos para manipular el estado.
-    fun onePressed(){
-        if(operacion == none){
-           display = digitize(display,1.0)
-        }
-        if(operacion == sum){
-            if(willClear == true){
-                willClear = false
-                operando1 = display
-                display = 1.0
-            }else{
-                display = digitize(display,1.0)
+    fun xd(digit: Double, clear: Boolean =  false){
+        when (operacion) {
+            none ->if(resultado == 0.0){
+                display = digitize(display,digit)
+            }else if(resultado !== 0.0) {
+                resultado = 0.0
+                acPressed()
+            }
+            else-> {
+                if(willClear){
+                    willClear = false
+                    operando1 = display
+                    display = digit
+                }else{
+                    display = digitize(display,digit)
+                }
             }
         }
     }
+    //metodos para manipular el estado.
+    fun onePressed(){
+        xd(1.0)
+    }
     fun twoPressed(){
-        if(operacion == none){
-            display = digitize(display,2.0)
-        }
+        xd(2.0)
+    }
+    fun threePressed(){
+        xd(3.0)
+    }
+    fun fourPressed(){
+        xd(4.0)
+    }
+    fun fivePressed(){
+        xd(5.0)
+    }
+    fun sixPressed(){
+        xd(6.0)
+    }
+    fun sevenPressed(){
+        xd(7.0)
+    }
+    fun eigthPressed(){
+        xd(8.0)
+    }
+    fun ninePressed(){
+        xd(9.0)
+    }
+    fun zeroPressed(){
+        xd(0.0)
     }
 
+
     fun sumPressed(){
+        if(resultado !== 0.0){
+            operando1 = display
+        }
         operacion = sum
         willClear = true
 
     }
 
-    fun equalsPressed(){
-        operando2 = display;
-        if(operacion == sum){
-            operando1 = operando1 + operando2
-            display = operando1
+    fun subtractionPressed(){
+        if(resultado !== 0.0){
+            operando1 = display
         }
+        operacion = subs
+        willClear = true
+
     }
 
+    fun multiplicationPressed(){
+        if(resultado !== 0.0){
+            operando1 = display
+        }
+        operacion = mul
+        willClear = true
 
+    }
+
+    fun divisionPressed(){
+        if(resultado !== 0.0){
+            operando1 = display
+        }
+        operacion = div
+        willClear = true
+
+    }
+
+    fun equalsPressed(){
+            operando2 = display;
+
+        if(operacion == sum){
+            resultado = operando1 + operando2
+            display = resultado
+             valueRest()
+        }
+        if(operacion == subs){
+            resultado = operando1 - operando2
+            display = resultado
+             valueRest()
+        }
+        if(operacion == mul){
+            resultado = operando1 * operando2
+            display = resultado
+             valueRest()
+        }
+        if(operacion == div){
+            if (operando2 !== 0.0){
+                resultado = operando1 / operando2
+            display = resultado
+                operando2 = 0.0
+            }
+            else display = 00000000000000000000.0
+        }
+         valueRest()
+
+    }
+
+    fun acPressed(){
+         display = 0.0
+         operando1 = 0.0
+         operando2 = 0.0
+         operacion=  0
+         willClear = false;
+    }
+
+    fun valueRest(){
+        operando1 = 0.0
+        operando2 = 0.0
+        operacion=  0
+        willClear = false;
+    }
 
     // metodos ayudantes
     fun digitize( numeroActual:Double, digito: Double ) : Double{
